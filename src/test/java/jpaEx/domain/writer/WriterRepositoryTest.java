e5d10b7a6ee9441a584bc01969ca31cafb5e6301
dasd <dasd412@naver.com>
2021-12-06 10:56:23 +0900

    단순 연관관계 저장 테스트 수행 마침. 다른 테스트도 적용 필요

--- src/test/java/jpaEx/domain/writer/WriterRepositoryTest.java
+++ src/test/java/jpaEx/domain/writer/WriterRepositoryTest.java
@@ -5,6 +5,7 @@ import jpaEx.domain.diary.DiabetesDiaryRepository;
 import jpaEx.domain.diet.Diet;
 import jpaEx.domain.diet.DietRepository;
 import jpaEx.domain.diet.EatTime;
+import jpaEx.domain.food.Food;
 import jpaEx.domain.food.FoodRepository;
 import org.junit.After;
 import org.junit.Test;
@@ -121,5 +122,48 @@ public class WriterRepositoryTest {
         assertThat(found.getDiaries().get(0).getDietList().get(0).getEatTime()).isEqualTo(diet.getEatTime());
         assertThat(found.getDiaries().get(0).getDietList().get(0).getBloodSugar()).isEqualTo(diet.getBloodSugar());
         logger.info(found.getDiaries().get(0).getDietList().get(0).toString());
+    }
+
+    @Transactional
+    @Test
+    public void saveWriterWithDiaryWithDietWithFood(){
+        //given
+
+        DiabetesDiary diary=new DiabetesDiary(20,"test", LocalDateTime.now());
+        Diet diet=new Diet(EatTime.Lunch,100,diary);
+        diary.addDiet(diet);
+
+        Writer me=new Writer("ME","TEST@NAVER.COM",Role.User);
+        me.addDiary(diary);
+
+        Food food=new Food("pizza",diet,me);
+        diet.addFood(food);
+        writerRepository.save(me);
+
+        //when
+        Writer found=writerRepository.findAll().get(0);
+
+        //then
+        //writer
+        assertThat(found).isEqualTo(me);
+        assertThat(found.getName()).isEqualTo(me.getName());
+        assertThat(found.getEmail()).isEqualTo(me.getEmail());
+        assertThat(found.getRole()).isEqualTo(me.getRole());
+        //diary
+        assertThat(found.getDiaries().get(0)).isEqualTo(diary);
+        assertThat(found.getDiaries().get(0).getFastingPlasmaGlucose()).isEqualTo(diary.getFastingPlasmaGlucose());
+        assertThat(found.getDiaries().get(0).getRemark()).isEqualTo(diary.getRemark());
+        logger.info(found.getDiaries().get(0).toString());
+
+        //diet
+        assertThat(found.getDiaries().get(0).getDietList().get(0)).isEqualTo(diet);
+        assertThat(found.getDiaries().get(0).getDietList().get(0).getEatTime()).isEqualTo(diet.getEatTime());
+        assertThat(found.getDiaries().get(0).getDietList().get(0).getBloodSugar()).isEqualTo(diet.getBloodSugar());
+        logger.info(found.getDiaries().get(0).getDietList().get(0).toString());
+
+        //food
+        assertThat(found.getDiaries().get(0).getDietList().get(0).getFoodList().get(0)).isEqualTo(food);
+        assertThat(found.getDiaries().get(0).getDietList().get(0).getFoodList().get(0).getFoodName()).isEqualTo(food.getFoodName());
+        logger.info(found.getDiaries().get(0).getDietList().get(0).getFoodList().get(0).toString());
     }
 }
