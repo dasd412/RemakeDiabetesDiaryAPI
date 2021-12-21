2e626f8f1d1e919415b24d4ac1c02d6f11785e5d
dasd <dasd412@naver.com>
2021-12-21 17:23:03 +0900

    findDiaryService 메서드 17 개 중에서 3개 컨트롤러와 매핑 및 컨트롤러 테스트 수행 성공

--- src/main/java/refactoringAPI/controller/diary/DiaryFindRestController.java
+++ src/main/java/refactoringAPI/controller/diary/DiaryFindRestController.java
@@ -6,16 +6,31 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import refactoringAPI.controller.ApiResult;
+import refactoringAPI.controller.diary.diabetesdiary.DiabetesDiaryFindResponseDTO;
+import refactoringAPI.controller.diary.diabetesdiary.DiaryListFindResponseDTO;
 import refactoringAPI.controller.diary.writer.WriterFindResponseDTO;
 import refactoringAPI.domain.diary.EntityId;
 import refactoringAPI.domain.diary.diabetesDiary.DiabetesDiary;
 import refactoringAPI.domain.diary.writer.Writer;
 import refactoringAPI.service.FindDiaryService;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController
 public class DiaryFindRestController {
     /*
     컨트롤러 계층에는 엔티티가 사용되선 안되며 DTO 로 감싸줘야 한다.
+     */
+
+    /*
+    Missing URI template variable for method parameter of type string 관련 에러는
+    @GetMapping() 내의 id 값과 파라미터로 들어가는 id가 이름이 서로 다를 경우에 발생한다.
+    해결 방법 중 하나는 @PathVariable()에 변수를 할당하는 것이다.
+
+    ex)
+    @GetMapping("/{userId}")
+    ->@PathVariable("userId") Long id
      */
     private final FindDiaryService findDiaryService;
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
@@ -25,10 +40,28 @@ public class DiaryFindRestController {
     }
 
     @GetMapping("api/diary/owner/diabetesDiary/{id}")
-    public ApiResult<WriterFindResponseDTO> findOwnerOfDiary(@PathVariable Long id) {
+    public ApiResult<WriterFindResponseDTO> findOwnerOfDiary(@PathVariable("id") Long diaryId) {
         logger.info("find owner of the diary");
-        return ApiResult.OK(new WriterFindResponseDTO(findDiaryService.getWriterOfDiary(EntityId.of(DiabetesDiary.class, id))));
+
+        return ApiResult.OK(new WriterFindResponseDTO(findDiaryService.getWriterOfDiary(EntityId.of(DiabetesDiary.class, diaryId))));
     }
 
+    @GetMapping("api/diary/diabetesDiary/list/owner/{id}")
+    public ApiResult<List<DiaryListFindResponseDTO>> findDiabetesDiariesOfOwner(@PathVariable("id") Long writerId) {
+        logger.info("find diabetes diaries of the owner");
 
+        List<DiabetesDiary> diabetesDiaryList = findDiaryService.getDiabetesDiariesOfWriter(EntityId.of(Writer.class, writerId));
+        List<DiaryListFindResponseDTO> dtoList = diabetesDiaryList.stream().map(
+                DiaryListFindResponseDTO::new
+        ).collect(Collectors.toList());
+
+        return ApiResult.OK(dtoList);
+    }
+
+    @GetMapping("api/diary/owner/{writerId}/diabetesDiary/{diaryId}")
+    public ApiResult<DiabetesDiaryFindResponseDTO> findOneDiabetesDiaryOfOwner(@PathVariable("writerId") Long writerId, @PathVariable("diaryId") Long diaryId) {
+        logger.info("find only one diabetes diary of the owner");
+
+        return ApiResult.OK(new DiabetesDiaryFindResponseDTO(findDiaryService.getDiabetesDiaryOfWriter(EntityId.of(Writer.class, writerId), EntityId.of(DiabetesDiary.class, diaryId))));
+    }
 }
--- src/test/java/refactoringAPI/controller/diary/DiaryFindRestControllerTest.java
+++ src/test/java/refactoringAPI/controller/diary/DiaryFindRestControllerTest.java
@@ -24,6 +24,7 @@ import refactoringAPI.controller.diary.writer.WriterJoinRequestDTO;
 import refactoringAPI.domain.diary.diet.EatTime;
 import refactoringAPI.domain.diary.writer.Role;
 
+import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -43,6 +44,10 @@ public class DiaryFindRestControllerTest {
     private MockMvc mockMvc;
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    /*
+    hasSize()는 json array 의 길이를 판단하는 메서드다.
+     */
 
     @Before
     public void setup() throws Exception {
@@ -89,8 +94,8 @@ public class DiaryFindRestControllerTest {
     @Test
     public void findInvalidOwnerOfDiary() throws Exception {
         //given
-        long invalidId=2L;
-        String url = "http://localhost:" + port +"api/diary/owner/diabetesDiary/"+invalidId;
+        long invalidId = 2L;
+        String url = "http://localhost:" + port + "api/diary/owner/diabetesDiary/" + invalidId;
 
         //when and then
         mockMvc.perform(get(url).contentType(MediaType.APPLICATION_JSON_UTF8))
@@ -102,8 +107,8 @@ public class DiaryFindRestControllerTest {
     @Test
     public void findOwnerOfDiary() throws Exception {
         //given
-        long validId=1L;
-        String url = "http://localhost:" + port +"api/diary/owner/diabetesDiary/"+validId;
+        long validId = 1L;
+        String url = "http://localhost:" + port + "api/diary/owner/diabetesDiary/" + validId;
 
         //when and then
         mockMvc.perform(get(url).contentType(MediaType.APPLICATION_JSON_UTF8))
@@ -115,5 +120,86 @@ public class DiaryFindRestControllerTest {
                 .andExpect(MockMvcResultMatchers.jsonPath("$.response.role").value("User"));
     }
 
+    @Transactional
+    @Test
+    public void findDiabetesDiariesOfWriter() throws Exception {
+        //given
+        DiabetesDiaryRequestDTO diaryRequestDTO1 = new DiabetesDiaryRequestDTO(1L, 200, "TEST@", "2021", "09", "26", "00", "00", "00");
+
+        String postDiaryUrl = "http://localhost:" + port + "api/diary/diabetesDiary";
+        mockMvc.perform(post(postDiaryUrl).contentType(MediaType.APPLICATION_JSON_UTF8)
+                        .content(new ObjectMapper().writeValueAsString(diaryRequestDTO1)))
+                .andExpect(status().isOk());
+
+        DiabetesDiaryRequestDTO diaryRequestDTO2 = new DiabetesDiaryRequestDTO(1L, 150, "TEST@@", "2021", "09", "27", "00", "00", "00");
+
+        mockMvc.perform(post(postDiaryUrl).contentType(MediaType.APPLICATION_JSON_UTF8)
+                        .content(new ObjectMapper().writeValueAsString(diaryRequestDTO2)))
+                .andExpect(status().isOk());
+
+        long writerId = 1L;
+        String url = "http://localhost:" + port + "api/diary/diabetesDiary/list/owner/" + writerId;
+
+        //when and then
+        mockMvc.perform(get(url).contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.success").value("true"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.response").value(hasSize(3)));
+    }
+
+    @Transactional
+    @Test
+    public void findDiaryOfInvalidOwner() throws Exception {
+        //given
+        WriterJoinRequestDTO dto = new WriterJoinRequestDTO("test2", "test2@naver.com", Role.User);
+        String url = "http://localhost:" + port + "/api/diary/writer";
+
+        mockMvc.perform(post(url).contentType(MediaType.APPLICATION_JSON_UTF8)
+                        .content(new ObjectMapper().writeValueAsString(dto)))
+                .andExpect(status().isOk()).andDo(print());
+
+        long writerId = 2L;
+        long id = 1L;
+        String targetUrl = "http://localhost:" + port + "api/diary/owner/" + writerId + "/diabetesDiary/" + id;
+
+        //when and then
+        mockMvc.perform(get(targetUrl).contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andDo(print())
+                .andExpect(status().isNotFound());
+    }
+
+    @Transactional
+    @Test
+    public void findInvalidDiaryOfOwner() throws Exception {
+        //given
+        long writerId = 1L;
+        long invalidId = 2L;
+        String url = "http://localhost:" + port + "api/diary/owner/" + writerId + "/diabetesDiary/" + invalidId;
+
+        //when and then
+        mockMvc.perform(get(url).contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andDo(print())
+                .andExpect(status().isNotFound());
+    }
+
+    @Transactional
+    @Test
+    public void findDiaryOfOwner() throws Exception {
+        //given
+        long writerId = 1L;
+        long invalidId = 1L;
+        String url = "http://localhost:" + port + "api/diary/owner/" + writerId + "/diabetesDiary/" + invalidId;
+
+        //when and then
+        mockMvc.perform(get(url).contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andDo(print())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.success").value("true"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.response.diaryId").value("1"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.response.writerId").value("1"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.response.fastingPlasmaGlucose").value("100"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.response.remark").value("TEST"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.response.writtenTime").value("2021-09-25T06:49:41"));
+    }
 
 }
