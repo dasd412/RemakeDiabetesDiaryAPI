76b1d95b1ca32ddda41704346d5cec5d62f8aed9
dasd <dasd412@naver.com>
2021-12-26 11:30:50 +0900

    식단 dto 미처 수정안한 것 수정

--- src/main/java/com/dasd412/remake/api/controller/diary/diet/DietListFindResponseDTO.java
+++ src/main/java/com/dasd412/remake/api/controller/diary/diet/DietListFindResponseDTO.java
@@ -7,10 +7,6 @@ import org.apache.commons.lang3.builder.ToStringStyle;
 
 public class DietListFindResponseDTO {
 
-    private final Long writerId;
-
-    private final Long diaryId;
-
     private final Long dietId;
 
     private final EatTime eatTime;
@@ -18,20 +14,11 @@ public class DietListFindResponseDTO {
     private final int bloodSugar;
 
     public DietListFindResponseDTO(Diet diet) {
-        this.writerId = diet.getDiary().getWriter().getId();
-        this.diaryId = diet.getDiary().getId();
         this.dietId = diet.getDietId();
         this.eatTime = diet.getEatTime();
         this.bloodSugar = diet.getBloodSugar();
     }
 
-    public Long getWriterId() {
-        return writerId;
-    }
-
-    public Long getDiaryId() {
-        return diaryId;
-    }
 
     public Long getDietId() {
         return dietId;
@@ -48,8 +35,6 @@ public class DietListFindResponseDTO {
     @Override
     public String toString() {
         return new ToStringBuilder(this, ToStringStyle.SHORT_PREFIX_STYLE)
-                .append("writer", writerId)
-                .append("diary", diaryId)
                 .append("diet", dietId)
                 .append("bloodSugar", bloodSugar)
                 .append("eatTime", eatTime)
