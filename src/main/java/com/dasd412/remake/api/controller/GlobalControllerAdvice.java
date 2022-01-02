7dcae3a0d489a661f4a4b09b13b1d456342c3d2b
dasd <dasd412@naver.com>
2022-01-02 22:46:08 +0900

    코드 중복을 제거하기 위해 컨트롤러 어드바이스 사용.

---
+++ src/main/java/com/dasd412/remake/api/controller/GlobalControllerAdvice.java
@@ -0,0 +1,23 @@
+package com.dasd412.remake.api.controller;
+
+import com.dasd412.remake.api.config.security.auth.PrincipalDetails;
+import com.dasd412.remake.api.domain.diary.writer.Writer;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+@ControllerAdvice
+public class GlobalControllerAdvice {
+
+    //모든 뷰 컨트롤러와 에러 페이지에서 공통적으로 세션정보를 ModelAttribute 하는 로직. 중복 코드를 줄일 수 있다.
+    @ModelAttribute
+    public void modelAddSessionInfo(Model model, @AuthenticationPrincipal PrincipalDetails principalDetails) {
+        if (principalDetails != null && principalDetails.getWriter() != null) {
+            Writer user = principalDetails.getWriter();
+            model.addAttribute("userPK", user.getId());
+            model.addAttribute("userNickName", user.getName());
+            model.addAttribute("userEmail", user.getEmail());
+        }
+    }
+}
--- src/main/java/com/dasd412/remake/api/controller/security/LoginController.java
+++ src/main/java/com/dasd412/remake/api/controller/security/LoginController.java
@@ -1,12 +1,8 @@
 package com.dasd412.remake.api.controller.security;
 
-import com.dasd412.remake.api.config.security.auth.PrincipalDetails;
-import com.dasd412.remake.api.domain.diary.writer.Writer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -15,26 +11,14 @@ public class LoginController {
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @GetMapping("/loginForm")
-    public String loginForm(Model model, @AuthenticationPrincipal PrincipalDetails principalDetails) {
+    public String loginForm() {
         logger.info("loginForm view resolve");
-        if (principalDetails != null && principalDetails.getWriter() != null) {
-            Writer user = principalDetails.getWriter();
-            model.addAttribute("userPK", user.getId());
-            model.addAttribute("userNickName", user.getName());
-            model.addAttribute("userEmail", user.getEmail());
-        }
         return "/login/loginForm";
     }
 
     @GetMapping("/joinForm")
-    public String joinForm(Model model, @AuthenticationPrincipal PrincipalDetails principalDetails) {
+    public String joinForm() {
         logger.info("joinForm view resolve");
-        if (principalDetails != null && principalDetails.getWriter() != null) {
-            Writer user = principalDetails.getWriter();
-            model.addAttribute("userPK", user.getId());
-            model.addAttribute("userNickName", user.getName());
-            model.addAttribute("userEmail", user.getEmail());
-        }
         return "/login/joinForm";
     }
 
