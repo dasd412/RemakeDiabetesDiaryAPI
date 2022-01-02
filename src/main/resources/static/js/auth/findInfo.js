00155477ef091ebbdf9634b1037eaf0fcdd9642d
dasd <dasd412@naver.com>
2022-01-02 14:17:20 +0900

    1. 로그인 폼 꾸미기 2. 라이브 리로드 적용 (css 등 바꿀 때 서버 다시 안띄워도 됨.)

---
+++ src/main/resources/static/js/auth/findInfo.js
@@ -0,0 +1,13 @@
+
+function findId(){
+
+}
+
+function findPassword(){
+
+}
+
+$(document).ready(function () {
+    $('#findIdBtn').attr('onclick', 'findId()');
+    $('#findPasswordBtn').attr('onclick','findPassword()');
+});
