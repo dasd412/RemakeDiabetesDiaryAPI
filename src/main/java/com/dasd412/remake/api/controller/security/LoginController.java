fcda639c65a38c93c87af0b0bdc82da82c0d3851
dasd <dasd412@naver.com>
2022-01-02 22:03:08 +0900

    로그인 폼이나 회원 가입 폼 볼때도 세션 정보 모델에 추가함. 이유는 세션 정보가 있을 경우 로그아웃이 떠야 하기 때문. 하지만 코드 중복이 심함.

--- src/main/java/com/dasd412/remake/api/controller/security/LoginController.java
+++ src/main/java/com/dasd412/remake/api/controller/security/LoginController.java
@@ -1,8 +1,12 @@
 package com.dasd412.remake.api.controller.security;
 
+import com.dasd412.remake.api.config.security.auth.PrincipalDetails;
+import com.dasd412.remake.api.domain.diary.writer.Writer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -11,14 +15,26 @@ public class LoginController {
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @GetMapping("/loginForm")
-    public String loginForm() {
+    public String loginForm(Model model, @AuthenticationPrincipal PrincipalDetails principalDetails) {
         logger.info("loginForm view resolve");
+        if (principalDetails != null && principalDetails.getWriter() != null) {
+            Writer user = principalDetails.getWriter();
+            model.addAttribute("userPK", user.getId());
+            model.addAttribute("userNickName", user.getName());
+            model.addAttribute("userEmail", user.getEmail());
+        }
         return "/login/loginForm";
     }
 
     @GetMapping("/joinForm")
-    public String joinForm() {
+    public String joinForm(Model model, @AuthenticationPrincipal PrincipalDetails principalDetails) {
         logger.info("joinForm view resolve");
+        if (principalDetails != null && principalDetails.getWriter() != null) {
+            Writer user = principalDetails.getWriter();
+            model.addAttribute("userPK", user.getId());
+            model.addAttribute("userNickName", user.getName());
+            model.addAttribute("userEmail", user.getEmail());
+        }
         return "/login/joinForm";
     }
 
