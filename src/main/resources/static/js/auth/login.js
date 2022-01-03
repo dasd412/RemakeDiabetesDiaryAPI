e5042b5ddd7bc2c67d14dbc9f59f40050b7cd735
dasd <dasd412@naver.com>
2022-01-03 15:51:30 +0900

    로그인 시, 아이디와 비밀번호 형식에 맞는 지 체크하는 로직 추가.

---
+++ src/main/resources/static/js/auth/login.js
@@ -0,0 +1,25 @@
+function validateId(username) {
+    return username.length>0;
+}
+
+function validatePw(password) {
+    return password.length>=7;
+}
+
+$(document).ready(function () {
+    $("#login-form").on('submit', function (e) {
+        e.preventDefault();
+
+        const username=$("#username").val();
+        const password=$("#password").val();
+        if (validateId(username)===false){
+            swal('',"공백으로 된 이름이에요.","error");
+            return;
+        }
+        if (validatePw(password)===false){
+            swal('',"비밀번호는 7자 이상이여야 해요.","error");
+            return;
+        }
+        this.submit();
+    });
+});
--- src/main/resources/static/js/auth/signup.js
+++ src/main/resources/static/js/auth/signup.js
@@ -1,73 +1,70 @@
-
-function validateEmail(str){
-	const regExp = /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
-	return regExp.test(str);
+function validateEmail(str) {
+    const regExp = /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
+    return regExp.test(str);
 }
 
-function submitSignup(){
-    const username=$("#username").val();
-    const password=$("#password").val();
-    const email=$("#email").val();
+function submitSignup() {
+    const username = $("#username").val();
+    const password = $("#password").val();
+    const email = $("#email").val();
 
     //front validation guard
-    if (username==null || username==""){
-        swal('',"공백으로 된 이름은 만들 수 없어요.","error");
+    if (username == null || username === "") {
+        swal('', "공백으로 된 이름은 만들 수 없어요.", "error");
         return;
     }
-    if (password==null||password==""){
-        swal('',"공백으로 된 패스워드는 만들 수 없어요.","error");
+    if (password == null || password === "") {
+        swal('', "공백으로 된 패스워드는 만들 수 없어요.", "error");
         return;
     }
-    if (password.length<=6){
-        swal('',"비밀번호는 7자 이상이여야 해요.","error");
+    if (password.length <= 6) {
+        swal('', "비밀번호는 7자 이상이여야 해요.", "error");
         return;
     }
-    if (email==null||email==""){
-        swal('',"공백으로 된 이메일은 만들 수 없어요.","error");
+    if (email == null || email === "") {
+        swal('', "공백으로 된 이메일은 만들 수 없어요.", "error");
         return;
     }
-    if (validateEmail(email)==false){
-        swal('',"올바른 형식의 이메일이 아니에요.","error");
+    if (validateEmail(email) === false) {
+        swal('', "올바른 형식의 이메일이 아니에요.", "error");
         return;
     }
 
 
-    const userData={
-        name:username,
-        password:password,
-        email:email
+    const userData = {
+        name: username,
+        password: password,
+        email: email
     };
 
     $.ajax({
-        type:"POST",
-        url:"/signup/user",
-        dataType:'json',
-        contentType:'application/json; charset=utf-8',
+        type: "POST",
+        url: "/signup/user",
+        dataType: 'json',
+        contentType: 'application/json; charset=utf-8',
         data: JSON.stringify(userData)
-    }).done(function(e){
+    }).done(function (e) {
 
         //회원가입 성공 시 처리
-        if(e.success==true){
-            swal('',"회원 가입 성공!","success");
-        //실패 시 alert message
-        }
-        else{
-            if(e.error.message=="duplicateName"){
-                swal('',"중복된 이름이에요.","error");
-            }
-            else if(e.error.message=="duplicateEmail"){
-                swal('',"중복된 이메일이에요.","error");
+        if (e.success === true) {
+            swal('', "회원 가입 성공!", "success");
+            //실패 시 alert message
+        } else {
+            if (e.error.message === "duplicateName") {
+                swal('', "중복된 이름이에요.", "error");
+            } else if (e.error.message === "duplicateEmail") {
+                swal('', "중복된 이메일이에요.", "error");
             }
         }
 
     });
 }
 
-function goBackLogin(){
-    window.location.href="/loginForm";
+function goBackLogin() {
+    window.location.href = "/loginForm";
 }
 
 $(document).ready(function () {
     $('#signupBtn').attr('onclick', 'submitSignup()');
-    $('#goLoginBtn').attr('onclick','goBackLogin()');
+    $('#goLoginBtn').attr('onclick', 'goBackLogin()');
 });
