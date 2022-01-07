0687cd0f94e03a6868927548a7eb0b8c4319cceb
dasd <dasd412@naver.com>
2022-01-07 17:13:43 +0900

    작성폼 꾸미기 1

--- src/main/resources/static/js/post/post.js
+++ src/main/resources/static/js/post/post.js
@@ -1,45 +1,56 @@
-function apiPostTest() {
+function addBreakFastFood() {
+    console.log('addBreakFastFood');
+    console.log($("#breakFastFoods li").length);
 
-    const data = {
-        writerId: 1,
-        fastingPlasmaGlucose: 100,
-        remark: "test",
-        year: "2022",
-        month: "01",
-        day: "03",
-        hour: "00",
-        minute: "00",
-        second: "00"
-    };
-
-    $.ajax({
-        type: 'POST',
-        url: 'api/diary/user/diabetes-diary',
-        dataType: 'json',
-        contentType: 'application/json;charset=utf-8',
-        data: JSON.stringify(data)
-    }).done(function () {
-        console.log("success");
-    }).fail(function () {
-        console.log("fail");
-    });
+    const breakFastFoodName=$("#breakFastFoodName").val();
+    $("#breakFastFoods").append("<li>" +
+        breakFastFoodName +
+        "<span class='close'>x</span></li>"
+        + "</li>");
 }
 
-function apiGetTest() {
-
-    $.ajax({
-        type: 'GET',
-        url: 'api/diary/user/diabetes-diary/list',
-        contentType: 'application/json; charset=utf-8',
-    }).done(function (data) {
-        console.log(data);
-    }).fail(function (data) {
-        console.log("fail");
-    });
+function addLunchFood() {
+    console.log('addLunchFood');
+    console.log($("#lunchFoods li").length);
+    const lunchFoodName=$("#lunchFoodName").val();
+    $("#lunchFoods").append("<li>" +
+        lunchFoodName
+        + "<span class='close'>x</span></li>"
+        + "</li>");
 }
 
+function addDinnerFood() {
+    console.log('addDinnerFood');
+    console.log($("#dinnerFoods li").length);
+    const dinnerFoodName=$("#dinnerFoodName").val();
+    $("#dinnerFoods").append("<li>" +
+        dinnerFoodName
+        + "<span class='close'>x</span></li>"
+        + "</li>");
+}
+
+
+function postDiary() {
+    console.log("post");
+}
+
+function goToBack() {
+    window.location.href = "/";
+}
 
 $(document).ready(function () {
-    $('#apiPostBtn').attr('onclick', 'apiPostTest()');
-    $('#apiGetBtn').attr('onclick', 'apiGetTest()');
+    // let closeButtons = document.getElementsByClassName("close");
+    // let i;
+    //
+    // for (i = 0; i < closeButtons.length; i++) {
+    //     closeButtons[i].addEventListener("click", function () {
+    //         this.parentElement.style.display = 'none';
+    //     });
+    // }
+
+    $("#breakFastFoodsAddBtn").attr('onclick', 'addBreakFastFood()');
+    $("#lunchFoodsAddBtn").attr('onclick', 'addLunchFood()');
+    $("#dinnerFoodsAddBtn").attr('onclick', 'addDinnerFood()');
+    $("#createBtn").attr('onclick', 'postDiary()');
+    $("#cancelBtn").attr('onclick', 'goToBack()');
 });
