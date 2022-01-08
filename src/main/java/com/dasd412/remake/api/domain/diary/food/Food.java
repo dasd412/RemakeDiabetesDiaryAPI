1aad5438a314e9b95cfa9709de8940263d0de535
dasd <dasd412@naver.com>
2022-01-08 11:48:58 +0900

    1. 음식 수량 제약조건 변경 2. js 리팩토링 3. 동적으로 생성되는 li 태그에 id , class 붙이기 4. li의 자식인 span에 closeList 이벤트 붙임.

--- src/main/java/com/dasd412/remake/api/domain/diary/food/Food.java
+++ src/main/java/com/dasd412/remake/api/domain/diary/food/Food.java
@@ -42,7 +42,7 @@ public class Food {
 
     public Food(EntityId<Food, Long> foodEntityId, Diet diet, String foodName, double amount) {
         checkArgument(foodName.length() > 0 && foodName.length() <= 50, "food name length should be between 1 and 50");
-        checkArgument(amount > 0 && amount <= 10000, "amount unit is gram. it should be between 1g and 10kg");
+        checkArgument(amount > 0 && amount <= 1000, "amount unit is gram. it should be between 1g and 1kg");
         this.foodId = foodEntityId.getId();
         this.diet = diet;
         this.foodName = foodName;
@@ -65,7 +65,7 @@ public class Food {
     }
 
     private void modifyAmount(double amount) {
-        checkArgument(amount > 0 && amount <= 10000, "amount unit is gram. it should be between 1g and 10kg");
+        checkArgument(amount > 0 && amount <= 1000, "amount unit is gram. it should be between 1g and 1kg");
         this.amount = amount;
     }
 
--- src/main/resources/static/js/post/post.js
+++ src/main/resources/static/js/post/post.js
@@ -1,16 +1,88 @@
-const postManipulator = {
-    breakFastFoods: [],
-    lunchFoods: [],
-    dinnerFoods: [],
+function FoodData(name, amount) {
+    this.name = name;
+    this.amount = amount;
+}
+
+const PostManipulator = {
+    breakFastFoods: null,
+    lunchFoods: null,
+    dinnerFoods: null,
+    foodDataDict: null,
+    dataLengthDict: null,
     init: function () {
+        //음식 이름 set (중복 방지)
+        this.breakFastFoods = new Set();
+        this.lunchFoods = new Set();
+        this.dinnerFoods = new Set();
+
+        //(음식 이름, 중량)의 딕셔너리
+        this.foodDataDict = {"breakFast": [], "lunch": [], "dinner": []};
+
+        //각 식사의 li id 지급용 딕셔너리 increment by 1
+        this.dataLengthDict = {"breakFast": 0, "lunch": 0, "dinner": 0};
+
         $("#breakFastFoodsAddBtn").attr('onclick', 'addFood(this)');
         $("#lunchFoodsAddBtn").attr('onclick', 'addFood(this)');
         $("#dinnerFoodsAddBtn").attr('onclick', 'addFood(this)');
-        $("#createBtn").attr('onclick', 'postDiary()');
-        $("#cancelBtn").attr('onclick', 'goToBack()');
-    },
-    cacheFoods: function () {
 
+        $("#createBtn").on('click', function () {
+            this.save();
+        });
+        $("#cancelBtn").on('click', function () {
+            this.goToBack();
+        });
+    },
+    cacheAddFoods: function (foodName, foodAmount, key) {
+        switch (key) {
+            case "breakFast":
+                this.breakFastFoods.add(foodName);
+                this.foodDataDict[key].push(new FoodData(foodName, foodAmount));
+                break;
+            case "lunch":
+                this.lunchFoods.add(foodName);
+                this.foodDataDict[key].push(new FoodData(foodName, foodAmount));
+                break;
+
+            case "dinner":
+                this.dinnerFoods.add(foodName);
+                this.foodDataDict[key].push(new FoodData(foodName, foodAmount));
+                break;
+        }
+
+        this.dataLengthDict[key] += 1
+    },
+    cacheRemoveFoods: function (e, key) {
+
+        if (this.dataLengthDict[key] > 0) {
+            this.dataLengthDict[key] -= 1;
+        }
+    },
+    alreadyHasFoodName: function (key, foodName) {
+        switch (key) {
+            case "breakFast":
+                if (this.breakFastFoods.has(foodName)) {
+                    swal('', "아침 식사에 중복된 이름이 존재해요.", "error");
+                    return true;
+                }
+                break;
+            case "lunch":
+                if (this.lunchFoods.has(foodName)) {
+                    swal('', "점심 식사에 중복된 이름이 존재해요.", "error");
+                    return true;
+                }
+                break;
+            case "dinner":
+                if (this.dinnerFoods.has(foodName)) {
+                    swal('', "저녁 식사에 중복된 이름이 존재해요.", "error");
+                    return true;
+                }
+                break;
+        }
+
+        return false;
+    },
+    getLiId: function (key) {
+        return this.dataLengthDict[key];
     },
     save: function () {
 
@@ -20,36 +92,56 @@ const postManipulator = {
     },
     delete: function () {
 
+    },
+    goToBack: function () {
+        window.location.href = "/";
     }
 }
 
-function closeList(e) {
-    e.parentElement.style.display = 'none';
+function closeList(span) {
+    //, key, foodName, foodAmount
+    console.log(span.parentElement.id, span.parentElement.className);
+
+    // PostManipulator.cacheRemoveFoods(e);
+    // e.parentElement.style.display = 'none';
 }
 
 function addFood(button) {
     let foodName;
     let ulName;
     let foodAmount;
+    let mealKey;
 
-    if (button.id === "breakFastFoodsAddBtn") {
-        foodName = $("#breakFastFoodName").val();
-        ulName = "#breakFastFoods";
-        foodAmount = $("#breakFastFoodAmount").val();
+    switch (button.id) {
+        case "breakFastFoodsAddBtn":
+            foodName = $("#breakFastFoodName").val();
+            ulName = "#breakFastFoods";
+            foodAmount = $("#breakFastFoodAmount").val();
+            mealKey = "breakFast";
+            break;
 
-        let breakFastLength = $("#breakFastFoods li").length;
-    } else if (button.id === "lunchFoodsAddBtn") {
-        foodName = $("#lunchFoodName").val();
-        ulName = "#lunchFoods";
-        foodAmount = $("#lunchFoodAmount").val();
+        case "lunchFoodsAddBtn":
+            foodName = $("#lunchFoodName").val();
+            ulName = "#lunchFoods";
+            foodAmount = $("#lunchFoodAmount").val();
+            mealKey = "lunch";
+            break;
 
-        let lunchLength = $("#lunchFoods li").length;
-    } else {
-        foodName = $("#dinnerFoodName").val();
-        ulName = "#dinnerFoods";
-        foodAmount = $("#dinnerFoodAmount").val();
+        case "dinnerFoodsAddBtn":
+            foodName = $("#dinnerFoodName").val();
+            ulName = "#dinnerFoods";
+            foodAmount = $("#dinnerFoodAmount").val();
+            mealKey = "dinner";
+            break;
+    }
+    if (!isNaN(foodName)) {
+        swal('', "음식 이름은 문자로 표현해야 해요", "error");
+        return;
+    }
 
-        let dinnerLength = $("#dinnerFoods li").length;
+    if (isNaN(foodAmount)) {
+        swal('', "음식 양은 숫자로 표현해야 해요", "error");
+        return;
     }
 
     if (foodName.length <= 0 || foodName.length > 50) {
@@ -57,28 +149,28 @@ function addFood(button) {
         return;
     }
 
-    if (foodAmount <= 0 || foodAmount > 10000) {
-        swal('', "음식 양은 1그램 이상 10키로 미만이여야 합니다.", "error");
+    if (foodAmount <= 0 || foodAmount > 1000) {
+        swal('', "음식 양은 1g 이상 1kg 미만이여야 합니다.", "error");
         return;
     }
 
-    $(ulName).append("<li>" +
-        foodName + " " + foodAmount + "g" +
-        "<span class='close'>x</span></li>"
-        + "</li>");
-    $(".close").attr('onclick', 'closeList(this)');
+    //중복된 데이터가 없을 경우에만 캐시 추가.
+    if (!PostManipulator.alreadyHasFoodName(mealKey, foodName)) {
+        const liId = PostManipulator.getLiId(mealKey);
 
-}
+        $(ulName).append("<li>" +
+            foodName + " "
+            + foodAmount + "g" +
+            "<span class='close' onclick='closeList(this)'>x</span></li>"
+            + "</li>");
+        const lastLi = $(ulName).children().last();
+        $(lastLi).attr('class', mealKey);
+        $(lastLi).attr('id', liId);
 
-
-function postDiary() {
-    console.log("post");
-}
-
-function goToBack() {
-    window.location.href = "/";
+        PostManipulator.cacheAddFoods(foodName, foodAmount, mealKey);
+    }
 }
 
 $(document).ready(function () {
-    postManipulator.init();
+    PostManipulator.init();
 });
