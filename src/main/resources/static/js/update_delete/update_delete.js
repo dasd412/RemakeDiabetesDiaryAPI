114520916c8f3bbec4a217aae22c9302a5bdb824
dasd <dasd412@naver.com>
2022-01-11 11:17:47 +0900

    document ready 할 때 있던 원본 저장용 캐시 생성.

--- src/main/resources/static/js/update_delete/update_delete.js
+++ src/main/resources/static/js/update_delete/update_delete.js
@@ -1,25 +1,20 @@
-const FoodData = {
-    id: null,
-    name: null,
-    amount: null,
+//기존 데이터 수정, 삭제 시 사용되는 생성자. 기존 데이터이기 때문에 PK가 중요하다.
+function OriginFoodData(id, name, amount) {
+    this.id = id;
+    this.name = name;
+    this.amount = amount;
+};
 
-    //기존 데이터 수정, 삭제 시 사용되는 생성자. 기존 데이터이기 때문에 PK가 중요하다.
-    initForOld: function (id, name, amount) {
-        this.id = id;
-        this.name = name;
-        this.amount = amount;
-    },
+//새로운 데이터 삽입 시 사용되는 생성자. 새로운 데이터는 db에 아직 없으므로 PK가 없다.
+function FoodData(name, amount) {
+    this.name = name;
+    this.amount = amount;
+};
 
-    //새로운 데이터 삽입 시 사용되는 생성자. 새로운 데이터는 db에 아직 없으므로 PK가 없다.
-    initForNew: function (name, amount) {
-        this.name = name;
-        this.amount = amount;
-    }
-}
 
 const UpdateDeleteManipulator = {
 
-    //document ready 했을 때 최초의 데이터들
+    //document ready 했을 때 최초의 데이터들 저장해 놓은 캐시.
     originalCache: {
         //일지 원본 데이터
         fastingPlasmaGlucose: $("#fastingPlasmaGlucose").val(),
@@ -28,17 +23,52 @@ const UpdateDeleteManipulator = {
         //식단 원본 데이터
         breakFastSugar: $("#breakFast").val(),
         lunchSugar: $("#lunch").val(),
-        dinnerSugar: $("#dinner").val()
+        dinnerSugar: $("#dinner").val(),
 
         //음식 원본 데이터
-
+        breakFastFoods: [],
+        lunchFoods: [],
+        dinnerFoods: []
     },
 
+    makeOriginFoodData: function (eatTime) {
+        let eachLiSelector;
+        let foodIdSelector;
+        let targetFoods;
+        switch (eatTime) {
+            case "breakFast":
+                eachLiSelector = "#breakFastFoods li";
+                foodIdSelector = ".breakFastFood";
+                targetFoods = this.originalCache.breakFastFoods;
+                break;
+            case "lunch":
+                eachLiSelector = "#lunchFoods li";
+                foodIdSelector = ".lunchFood";
+                targetFoods = this.originalCache.lunchFoods;
+                break;
+            case "dinner":
+                eachLiSelector = "#dinnerFoods li";
+                foodIdSelector = ".dinnerFood";
+                targetFoods = this.originalCache.dinnerFoods;
+                break;
+        }
+
+        $(eachLiSelector).each(function () {
+            const foodId = $(this).children(foodIdSelector).val();
+            $(this).children(".foodElement").each(function () {
+                const foodName = $(this).children(".child.foodName").text();
+                const amount = $(this).children(".child.amount").text();
+                targetFoods.push(new OriginFoodData(foodId, foodName, amount));
+            });
+        });
+
+    },
     init: function () {
         const _this = this;
 
-        console.log("origin");
-        console.log(this.originalCache);
+        this.makeOriginFoodData("breakFast");
+        this.makeOriginFoodData("lunch");
+        this.makeOriginFoodData("dinner");
 
         $("#updateBtn").on('click', function () {
             _this.update();
@@ -75,7 +105,7 @@ const UpdateDeleteManipulator = {
     }
 }
 
-function modifyFood(){
+function modifyFood() {
 
 }
 
