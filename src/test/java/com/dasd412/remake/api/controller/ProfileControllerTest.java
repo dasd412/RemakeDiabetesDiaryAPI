d89cd7fffb9b94a561dd1c2dbe0ba4872cf8c7f4
dasd <dasd412@naver.com>
2022-01-21 11:47:23 +0900

    배포할 때 포트넘버 지정하는 컨트롤러 추가

---
+++ src/test/java/com/dasd412/remake/api/controller/ProfileControllerTest.java
@@ -0,0 +1,41 @@
+package com.dasd412.remake.api.controller;
+
+import org.junit.Test;
+import org.springframework.mock.env.MockEnvironment;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ProfileControllerTest {
+
+    @Test
+    public void selectRealProfile() {
+        //given
+        String expectedProfile = "real";
+        MockEnvironment env = new MockEnvironment();
+        env.addActiveProfile(expectedProfile);
+        env.addActiveProfile("oauth");
+        env.addActiveProfile("real-db");
+
+        ProfileController controller = new ProfileController(env);
+
+        //when
+        String profile = controller.profile();
+
+        //then
+        assertThat(profile).isEqualTo(expectedProfile);
+    }
+
+    @Test
+    public void expectDefaultProfiles() {
+        //given
+        String expectedProfile = "default";
+        MockEnvironment env = new MockEnvironment();
+        ProfileController controller = new ProfileController(env);
+
+        //when
+        String profile = controller.profile();
+
+        //then
+        assertThat(profile).isEqualTo(expectedProfile);
+    }
+}
