e2d76290688771d53c19a6c049e971195b5a975d
dasd <dasd412@naver.com>
2022-01-21 12:09:50 +0900

    프로파일 컨트롤러 추가 후 500 에러 나오는 것 수정 시도

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,7 +7,7 @@ echo ">build 파일 복사"
 cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 현재 구동중인 애플리케이션 pid 확인"
-CURRENT_PID=$(pgrep -fl $ARTIFACT_ID | grep jar | awk '{print $1}')
+CURRENT_PID=$(ps -ef | grep ${ARTIFACT_ID} | grep jar | awk '{print $2}')
 
 echo "> 현재 구동중인 애플리케이션 pid: $CURRENT_PID"
 
