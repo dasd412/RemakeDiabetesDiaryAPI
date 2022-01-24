6a2550f34bc48870a3e7aaaae2aa23503f34393e
dasd <dasd412@naver.com>
2022-01-24 11:33:36 +0900

    1.각 메뉴 진입 경로 설정 2.각 메뉴 화면 뼈대 작성

--- src/main/java/com/dasd412/remake/api/controller/security/domain_view/ChartFormController.java
+++ src/main/java/com/dasd412/remake/api/controller/security/domain_view/ChartFormController.java
@@ -24,9 +24,66 @@ public class ChartFormController {
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
+    /**
+     * @return 차트 메뉴 화면
+     */
     @GetMapping("/chart-menu")
     public String showChartMenu() {
         logger.info("chart menu view resolve");
         return "/chart/chartMenu";
+    }
+
+    /**
+     * @return 혈당 추이 화면
+     */
+    @GetMapping("/chart-menu/chart/transition")
+    public String showChartTransition() {
+        logger.info("chart transition resolve");
+        return "/chart/transition";
+    }
+
+    /**
+     * @return 기간 내 공복 혈당 화면
+     */
+    @GetMapping("/chart-menu/chart/fasting-plasma-glucose")
+    public String showChartFastingPlasmaGlucose() {
+        logger.info("chart fasting-plasma-glucose resolve");
+        return "/chart/fastingPlasmaGlucose";
+    }
+
+    /**
+     * @return 기간 내 식사 혈당 화면
+     */
+    @GetMapping("/chart-menu/chart/blood-sugar/between-time")
+    public String showChartBloodSugarBetweenTime() {
+        logger.info("chart blood sugar between time resolve");
+        return "/chart/bloodSugarBetweenTime";
+    }
+
+    /**
+     * @return 식사 기준 식사 혈당 화면
+     */
+    @GetMapping("/chart-menu/chart/blood-sugar/eat-time")
+    public String showChartBloodSugarEatTime() {
+        logger.info("chart blood sugar eat time resolve");
+        return "/chart/bloodSugarEatTime";
+    }
+
+    /**
+     * @return 평균 혈당 화면
+     */
+    @GetMapping("/chart-menu/chart/average")
+    public String showChartAverage() {
+        logger.info("chart average resolve");
+        return "/chart/average";
+    }
+
+    /**
+     * @return 음식 화면
+     */
+    @GetMapping("/chart-menu/chart/food")
+    public String showChartFood() {
+        logger.info("chart food resolve");
+        return "/chart/food";
     }
 }
