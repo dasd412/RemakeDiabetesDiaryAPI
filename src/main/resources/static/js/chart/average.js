c76e0dcbd83e1898c803752c613345eea1a6216d
dasd <dasd412@naver.com>
2022-02-03 17:03:01 +0900

    평균 혈당 프론트 화면 재설계
    dto 및 컨트롤러 메서드 변경 필요
    dto에 공복 혈당, 각 식사 혈당, 전체 식사 혈당 한꺼번에 넣는 걸로 바꿔야 함.

---
+++ src/main/resources/static/js/chart/average.js
@@ -0,0 +1,7 @@
+/*
+ * @(#)bloodSugar.js        1.0.4 2022/2/3
+ *
+ * Copyright (c) 2022 YoungJun Yang.
+ * ComputerScience, ProgrammingLanguage, JavaScript, Pocheon-si, KOREA
+ * All rights reserved.
+ */
