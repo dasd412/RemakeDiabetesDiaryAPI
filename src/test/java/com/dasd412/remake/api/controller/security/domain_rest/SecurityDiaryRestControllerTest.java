071ba9ee04774f7c6f3b24b3d11448ab6cb63ed3
dasd <dasd412@naver.com>
2022-02-05 13:36:22 +0900

    음식 수량 단위 추가한 컨트롤러 코드 테스트 작성 및 수행 성공

--- src/test/java/com/dasd412/remake/api/controller/security/domain_rest/SecurityDiaryRestControllerTest.java
+++ src/test/java/com/dasd412/remake/api/controller/security/domain_rest/SecurityDiaryRestControllerTest.java
@@ -19,6 +19,7 @@ import com.dasd412.remake.api.domain.diary.diabetesDiary.DiabetesDiary;
 import com.dasd412.remake.api.domain.diary.diabetesDiary.DiaryRepository;
 import com.dasd412.remake.api.domain.diary.diet.Diet;
 import com.dasd412.remake.api.domain.diary.diet.DietRepository;
+import com.dasd412.remake.api.domain.diary.food.AmountUnit;
 import com.dasd412.remake.api.domain.diary.food.Food;
 import com.dasd412.remake.api.domain.diary.food.FoodRepository;
 import com.dasd412.remake.api.domain.diary.writer.Role;
@@ -40,6 +41,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
@@ -207,6 +209,42 @@ public class SecurityDiaryRestControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.success").value("true"))
                 .andExpect(jsonPath("$.response.id").value(1));
+
+        logger.info("repository \n");
+        DiabetesDiary found = diaryRepository.findDiabetesDiaryOfWriterWithRelation(1L, 1L).orElseThrow(NoResultException::new);
+        List<Food> foodList = foodRepository.findAll();
+
+        assertThat(found.getFastingPlasmaGlucose()).isEqualTo(100);
+        assertThat(found.getRemark()).isEqualTo("test");
+        assertThat(found.getDietList().size()).isEqualTo(3);
+        assertThat(foodList.size()).isEqualTo(breakFast.size() + lunch.size() + dinner.size());
+
+    }
+
+    @Test
+    public void postDiaryWithFoodAmountUnit() throws Exception {
+        //given
+        String url = "/api/diary/user/diabetes-diary";
+        List<SecurityFoodDTO> breakFast = IntStream.rangeClosed(1, 3).mapToObj(i -> new SecurityFoodDTO("breakFast" + i, i, AmountUnit.g))
+                .collect(Collectors.toList());
+        List<SecurityFoodDTO> lunch = IntStream.rangeClosed(1, 3).mapToObj(i -> new SecurityFoodDTO("lunch" + i, i, AmountUnit.mL))
+                .collect(Collectors.toList());
+        List<SecurityFoodDTO> dinner = IntStream.rangeClosed(1, 1).mapToObj(i -> new SecurityFoodDTO("dinner" + i, i, AmountUnit.count))
+                .collect(Collectors.toList());
+
+        SecurityDiaryPostRequestDTO dto = SecurityDiaryPostRequestDTO.builder().fastingPlasmaGlucose(100).remark("test")
+                .year("2021").month("12").day("22").hour("00").minute("00").second("00")
+                .breakFastSugar(110).lunchSugar(120).dinnerSugar(130)
+                .breakFastFoods(breakFast).lunchFoods(lunch).dinnerFoods(dinner).build();
+
+        //when and then
+        mockMvc.perform(post(url).with(user(principalDetails))
+                        .contentType(MediaType.APPLICATION_JSON_UTF8)
+                        .content(new ObjectMapper().writeValueAsString(dto)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.success").value("true"))
+                .andExpect(jsonPath("$.response.id").value(1));
+
         logger.info("repository \n");
         DiabetesDiary found = diaryRepository.findDiabetesDiaryOfWriterWithRelation(1L, 1L).orElseThrow(NoResultException::new);
         List<Food> foodList = foodRepository.findAll();
