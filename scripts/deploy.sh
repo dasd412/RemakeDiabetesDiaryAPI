f5a71eea3a76810c8e02427d698317ecf7d30126
dasd <dasd412@naver.com>
2022-03-06 14:37:45 +0900

    쉘 스크립트 오타 핫 픽스

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -31,6 +31,6 @@ chmod +x $JAR_NAME
 echo ">$JAR_NAME 실행"
 
 nohup java -jar \
-    -Dspring.config.location=classpath:/application.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties, /home/ec2-user/app/application-email.properties \
+    -Dspring.config.location=classpath:/application.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties,/home/ec2-user/app/application-email.properties \
 		-Dspring.profiles.active=real \
 		 $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
