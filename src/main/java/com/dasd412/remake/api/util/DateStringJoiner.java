734c4823d54524d4a74e86b7085cc46505fde295
dasd <dasd412@naver.com>
2022-07-23 12:00:01 +0900

    유틸 클래스 주석 제거

--- src/main/java/com/dasd412/remake/api/util/DateStringJoiner.java
+++ src/main/java/com/dasd412/remake/api/util/DateStringJoiner.java
@@ -1,5 +1,5 @@
 /*
- * @(#)DateStringJoiner.java        1.0.8 2022/2/16
+ * @(#)DateStringJoiner.java
  *
  * Copyright (c) 2022 YoungJun Yang.
  * ComputerScience, ProgrammingLanguage, Java, Pocheon-si, KOREA
@@ -16,9 +16,6 @@ import java.time.format.DateTimeFormatter;
 
 /**
  * 년도,월,일,시,분,초에 해당하는 문자열을 LocalDateTime으로 변환하기 위한 객체
- *
- * @author 양영준
- * @version 1.0.8 2022년 2월 16일
  */
 @RequiredArgsConstructor
 @Builder
--- src/main/java/com/dasd412/remake/api/util/RegexChecker.java
+++ src/main/java/com/dasd412/remake/api/util/RegexChecker.java
@@ -1,5 +1,5 @@
 /*
- * @(#)RegexChecker.java        1.1.2 2022/3/2
+ * @(#)RegexChecker.java
  *
  * Copyright (c) 2022 YoungJun Yang.
  * ComputerScience, ProgrammingLanguage, Java, Pocheon-si, KOREA
@@ -10,12 +10,6 @@ package com.dasd412.remake.api.util;
 
 import java.util.regex.Pattern;
 
-/**
- * 이메일 등 정규식 체킹용 유틸리티.
- *
- * @author 양영준
- * @version 1.1.2 2022년 3월 2일
- */
 public class RegexChecker {
 
     public static final String EMAIL_PATTERN = "^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$";
